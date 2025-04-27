0fdf8b54976676dea0cbcb2dc7bf17f3b9d17438
rydeblack <ryhad.boughanmi@gmail.com>
2025-04-27 10:52:18 +0200

    init:initialize repo with hello Java world

---
+++ src/ej/Main.java
@@ -0,0 +1,15 @@
+package ej;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num;
+        num = 5;
+        final int immuable = 20;
+        String language = "Java";
+        String sentence = language + " is a programming language.";
+        System.out.println(sentence);
+        String helloWorld = "Hello World";
+        System.out.println(helloWorld);
+    }
+}
