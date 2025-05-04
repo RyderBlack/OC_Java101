1e0c9db39a1e0ea750cdf0ee36c3b20b6084b96f
rydeblack <ryhad.boughanmi@gmail.com>
2025-05-04 14:07:38 +0200

    feat:add getter and setter to class Bloc

--- src/ej/Bloc.java
+++ src/ej/Bloc.java
@@ -3,10 +3,20 @@ package ej;
 public class Bloc {
 
     String description;
+
     Bloc() {
         this.description = "Hello from the Bloc!";
     }
+
     void displayDescription() {
         System.out.println(description);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
